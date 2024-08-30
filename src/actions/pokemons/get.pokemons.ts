b98f6ff64a147e89a58a12fa902725e0fc5c36ca
Camilo López <camiloj.lopez79@gmail.com>
2024-08-30 13:04:29 -0400

    Interfaces de PokeAPI

--- src/actions/pokemons/get.pokemons.ts
+++ src/actions/pokemons/get.pokemons.ts
@@ -1,19 +1,34 @@
 // Api
 import { pokeApi } from '../../config/api';
 // Entities
-import { Pokemon } from '../../domain/entities';
+import type { Pokemon } from '../../domain/entities';
+// Interfaces
+import type {
+  PokeAPIPaginatedResponse,
+  PokeAPIPokemon
+} from '../../infrastructure/interfaces';
 
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (
+  page: number,
+  limit: number = 20
+): Promise<Pokemon[]> => {
   try {
-    const url = '/pokemon';
-    const { data } = await pokeApi.get( url );
+    const url = `/pokemon?offset=${ page * 10 }&limit=${ limit }`;
+    const { data } = await pokeApi.get<PokeAPIPaginatedResponse>( url );
+
+    const pokemonPromises = data.results.map( ( info ) => {
+      return pokeApi.get<PokeAPIPokemon>( info.url );
+    });
+
+    const pokeApiPokemons = await Promise.all( pokemonPromises );
 
     console.log( data );
 
     return [];
   
   } catch ( error ) {
+    console.log( error );
     throw new Error( 'Error getting pokemons' );
   }
 }
