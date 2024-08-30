4bbbc6c60ec0f2e326f185aa60f4c3a1e9b59e6c
Camilo López <camiloj.lopez79@gmail.com>
2024-08-30 14:53:30 -0400

    Diseño del HomeScreen Parte 2

---
+++ src/config/theme/global-theme.ts
@@ -0,0 +1,9 @@
+// React Native
+import { StyleSheet } from 'react-native';
+
+
+export const globalTheme = StyleSheet.create({
+  globalMargin: {
+    marginHorizontal: 20
+  }
+});
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,30 +1,51 @@
 // React Native
 import {
+  FlatList,
   StyleSheet,
   View
 } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 // React Native Paper
 import {
-  ActivityIndicator,
-  Button,
   Text
 } from 'react-native-paper';
+// TanStack Query
 import { useQuery } from '@tanstack/react-query';
+// Actions
 import { getPokemons } from '../../../actions/pokemons';
 // Components
-import { PokeballBG } from '../../components';
+import {
+  PokeballBG,
+  PokemonCard
+} from '../../components';
+// Styles
+import { globalTheme } from '../../../config/theme';
 
 
 export const HomeScreen = () => {
-  const { isLoading, data = [] } = useQuery({
+  const { top } = useSafeAreaInsets();
+  const { isLoading, data: pokemons = [] } = useQuery({
     queryKey: [ 'pokemons' ],
     queryFn: () => getPokemons( 0 ),
     staleTime: 1000 * 60 * 60, // 60 min
   });
 
   return (
-    <View>
+    <View style={ globalTheme.globalMargin }>
       <PokeballBG style={ styles.imgPosition } />
+
+      <FlatList
+        data={ pokemons }
+        keyExtractor={ ( pokemon, index ) => `${ pokemon.id }-${ index }` }
+        numColumns={ 2 }
+        style={{ paddingTop: top }}
+        ListHeaderComponent={ () => (
+          <Text variant='displayMedium'>Pokédex</Text>
+        )}
+        renderItem={ ({ item: pokemon }) => (
+          <PokemonCard pokemon={ pokemon } />
+        )}
+      />
     </View>
   );
 }
